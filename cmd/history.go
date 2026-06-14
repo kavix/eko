136cedfb915d0173de63983f29d83698c461ee1d
kavix <kavix@yahoo.com>
2026-06-15 00:53:55 +0530

    Create history.go

---
+++ cmd/history.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"fmt"
+	"eko/internal/db"
+
+	"github.com/spf13/cobra"
+)
+
+var historyCmd = &cobra.Command{
+	Use:   "history",
+	Short: "Show snapshots",
+	Run: func(cmd *cobra.Command, args []string) {
+		database := db.InitDB()
+		rows, _ := database.Query("SELECT id, created_at FROM snapshots")
+		for rows.Next() {
+			var id, created string
+			rows.Scan(&id, &created)
+			fmt.Println(id, created)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(historyCmd)
+}
